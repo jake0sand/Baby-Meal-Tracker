6eb1a0801a03b5e6eb6b79a50c5eb7d6683520ec
jake0sand <79411811+jake0sand@users.noreply.github.com>
2022-02-20 22:23:17 -0700

    inserting entries and reading all entries functional. changed color theme.

---
+++ app/src/main/java/com/jakey/simplefeedingtracker/presentation/list/FeedingsListAdapter.kt
@@ -0,0 +1,51 @@
+package com.jakey.simplefeedingtracker.presentation.list
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.jakey.simplefeedingtracker.data.Feeding
+import com.jakey.simplefeedingtracker.databinding.FeedingItemBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
+class FeedingsListAdapter: RecyclerView.Adapter<FeedingsListAdapter.FeedingViewHolder>() {
+
+    private var feedingList = emptyList<Feeding>()
+
+    class FeedingViewHolder(val binding: FeedingItemBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedingViewHolder {
+        val binding = FeedingItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return FeedingViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: FeedingViewHolder, position: Int) {
+        val currentItem = feedingList[position]
+        holder.apply {
+            binding.tvDay.text = capitalize(currentItem.day)
+            binding.tvTime.text = currentItem.time
+            binding.tvAmount.text = currentItem.amount + " oz"
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return feedingList.size
+    }
+
+    fun setData(feeding: List<Feeding>) {
+        this.feedingList = feeding
+        notifyDataSetChanged()
+    }
+
+    private fun capitalize(str: String): String {
+        return str.replaceFirstChar {
+            if (it.isLowerCase()) it.titlecase(
+                Locale.getDefault()
+            ) else it.toString()
+        }
+    }
+
+//    var sdf: SimpleDateFormat = SimpleDateFormat("dd/MM/yyyy")
+//    var c: Calendar = Calendar.getInstance()
+//    var date: String = sdf.format(c.getTime())
+}
